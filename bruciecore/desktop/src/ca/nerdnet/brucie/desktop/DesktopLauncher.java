24a6027bffe9a1da7f0fb874ef22de19e37ae018
Colin Hahn <colin.hahn@sasktel.net>
2018-05-22 01:30:06 -0600

    Initial commit of BrucieCore skeleton/library.

---
+++ bruciecore/desktop/src/ca/nerdnet/brucie/desktop/DesktopLauncher.java
@@ -0,0 +1,31 @@
+package ca.nerdnet.brucie.desktop;
+
+import ca.nerdnet.brucie.core.BrucieConfig;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.nerdnet.brucie.BrucieCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		// This sets the window size
+		// Future: matching up resolutions becomes important in some situations.
+		//   some kind of resolution management might be nice here
+		config.width = 1024;
+		config.height = 680;
+
+		// Create BrucieConfig instance. This remains the same across platforms at
+		// this time, but does not need to.
+		BrucieConfig bconfig = new BrucieConfig();
+
+		// Create game
+		BrucieCore game = new BrucieCore(bconfig);
+
+		// Register any dynamic features
+		//game.registerFeature("NULL");
+
+		// Launch the LibGDX app
+		new LwjglApplication(game, config);
+	}
+}
