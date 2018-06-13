fe69766d2b12dc91f09e0dfcc50eab44f0337ccb
Colin Hahn <colin.hahn@sasktel.net>
2018-06-13 12:20:57 -0600

    Sanitizing

--- bruciecore/core/src/ca/nerdnet/brucie/unablox/terrain/TerrainData.java
+++ bruciecore/core/src/ca/nerdnet/brucie/unablox/terrain/TerrainData.java
@@ -1,4 +1,20 @@
 package ca.nerdnet.brucie.unablox.terrain;
 
 public class TerrainData {
+
+    private final byte[] dataMatrix;
+    private int dimension;
+
+    public TerrainData(int dim) {
+        dimension = dim;
+        dataMatrix = new byte[dim * dim];
+    }
+
+    public byte getCell(int x, int y) {
+        return dataMatrix[x + y * dimension];
+    }
+
+    public void putCell(int x, int y, byte value) {
+        dataMatrix[x + y * dimension] = value;
+    }
 }
--- bruciecore/core/src/ca/nerdnet/brucie/unablox/terrain/TerrainRenderer.java
+++ bruciecore/core/src/ca/nerdnet/brucie/unablox/terrain/TerrainRenderer.java
@@ -1,4 +1,18 @@
 package ca.nerdnet.brucie.unablox.terrain;
 
-public class TerrainRenderer {
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.RenderableProvider;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+
+public class TerrainRenderer implements RenderableProvider {
+
+
+
+
+
+    @Override
+    public void getRenderables(Array<Renderable> renderables, Pool<Renderable> pool) {
+
+    }
 }
